3688997632e976661acdcb1692ee1857f482d641
fawahkhan <khanmohdfawah21@gmail.com>
2025-01-21 05:07:25 +0530

    layout done

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from "./components/Navbar"; 
+import Banner from './components/Banner';
+function App() {
+  return(<>
+  <Navbar/>
+  <Banner/>
+  </>);
+}
+
+export default App;
---
+++ Frontend/src/components/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Banner() {
+  return (
+  <>
+  <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex">
+    <div className="w-full md:w-1/2">
+    <h1 className="text-2xl md:text-4xl font-bold">Opening
+    Doors to <span className="text-pink-500">Education for All!</span></h1>
+    </div>
+    <div className="w-full md:w-1/2">Right</div>
+  </div>
+  </>
+  );
+}
+
+export default Banner
