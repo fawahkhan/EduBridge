f9c76908353fc84e85e551155022a27d64c160e4
Mohd Fawah Khan <khanmohdfawah21@gmail.com>
2025-01-27 11:51:32 +0000

    undo commit

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
         phoneNumber:data.phoneNumber,
         password:data.password,
     }
-    await axios.post("https://edubridge-backend-gzov.onrender.com/user/login" , userInfo )
+    await axios.post("http://localhost:4001/user/login" , userInfo )
     .then((res)=>{
         console.log(res.data)
         if(res.data){
