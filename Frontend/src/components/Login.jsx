a5d23ce20e49b9cbbba8332b804bb0c92d8fd843
Mohd Fawah Khan <khanmohdfawah21@gmail.com>
2025-01-27 14:31:57 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
         phoneNumber:data.phoneNumber,
         password:data.password,
     }
-    await axios.post("http://localhost:4001/user/login" , userInfo )
+    await axios.post("https://edubridge-backend-gzov.onrender.com" , userInfo )
     .then((res)=>{
         console.log(res.data)
         if(res.data){
